82376af99c78de79afcdf07ac14366d29f9da849
arorasakshi1 <arorasakshi25392@gmail.com>
2020-08-12 19:22:15 +0530

    sakshi

---
+++ src/my_client.java
@@ -0,0 +1,28 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class my_client {
+
+	public static void main(String[] args) throws Exception, IOException {
+		// TODO Auto-generated method stub
+		Socket s=new Socket("localhost",1234);
+		DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+		dout.writeUTF("hello i am client and my name is sakshi");
+		dout.flush();
+		dout.close();
+		s.close();
+
+	}
+
+}
+
+
+//port no
+//ip address
+// socket
+//protocol
+// connection oriented tcp
+// connection less udp
+// mac adress
---
+++ src/my_server.java
@@ -0,0 +1,26 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class my_server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			ServerSocket ss= new ServerSocket(1234);
+			Socket s=ss.accept();
+			DataInputStream din=new DataInputStream(s.getInputStream());
+			String str=(String)din.readUTF();
+			System.out.println("client says:"+str);
+			ss.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
